6307a3d02144aefc410d9a0aea485efd456c6265
Anders Christiansen Sørby <anders@sorby.xyz>
2020-12-13 12:02:45 +0100

    Add CORS and GameOfLife

--- src/bin/backend.rs
+++ src/bin/backend.rs
@@ -4,6 +4,31 @@
 #[macro_use]
 extern crate rocket;
 
+use rocket::fairing::{Fairing, Info, Kind};
+use rocket::http::Header;
+use rocket::{Request, Response};
+
+pub struct CORS();
+
+impl Fairing for CORS {
+    fn info(&self) -> Info {
+        Info {
+            name: "Add CORS headers to requests",
+            kind: Kind::Response,
+        }
+    }
+
+    fn on_response(&self, request: &Request, response: &mut Response) {
+        response.set_header(Header::new("Access-Control-Allow-Origin", "*"));
+        response.set_header(Header::new(
+            "Access-Control-Allow-Methods",
+            "POST, GET, PATCH, OPTIONS",
+        ));
+        response.set_header(Header::new("Access-Control-Allow-Headers", "*"));
+        response.set_header(Header::new("Access-Control-Allow-Credentials", "true"));
+    }
+}
+
 // Create route / that returns "Hello, world!"
 #[get("/")]
 fn index() -> &'static str {
--- www/index.js
+++ www/index.js
@@ -1,3 +1,11 @@
 import * as wasm from "rust-play";
 
-wasm.greet("Anders");
+//wasm.greet("Anders");
+const Http = new XMLHttpRequest();
+const url='http://localhost:5000';
+Http.open("GET", url);
+Http.send();
+
+Http.onreadystatechange = (e) => {
+  console.log(Http.responseText)
+}
